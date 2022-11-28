7f13537c8de74e3437995c76fa6b1fbb7b8d88b6
Thomas Barth <thomasbarth@live.no>
2022-11-28 16:42:29 +0100

    h

--- tilnærmet integral.py
+++ tilnærmet integral.py
@@ -1,26 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
 import math
 def f(x): 
-  return 3*math.e**(-x**2/2)
+  return 3*math.sqrt(x)
   
 areal_verdier = []
-start_verdi = -2
+areal_trapes = []
+start_verdi = 1
 slutt_verdi = 2
-ant_rek = 50
+ant_rek = 100
 bredde_rek = (slutt_verdi-start_verdi)/ant_rek
-rektangel_startpos = 0* bredde_rek
+rektangel_startpos = bredde_rek*0
+## 0 = venstremetode, 0.5 = midt, 1= høyre
 
-for i in range (ant_rek):
+for i in range(ant_rek): ##rektangelmetode
   a = f(i*bredde_rek+rektangel_startpos+start_verdi)*bredde_rek
   areal_verdier.append(a)
+
+for i in range (ant_rek): ##trapesmetode
+    a= f(start_verdi+(i-1)*bredde_rek)*bredde_rek
+    areal_trapes.append(a)
  
 
 
 
 
 print(sum(areal_verdier))
+print(sum(areal_trapes))
