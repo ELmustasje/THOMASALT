12c2c64c51c54acda697a368bf8bcf83f9682133
Thomas Barth <thomasbarth@live.no>
2023-09-01 00:09:28 +0300

    h

--- .vs/THOMASALT/inf140/MA1/thbar6999-Q6.py
+++ .vs/THOMASALT/inf140/MA1/thbar6999-Q6.py
@@ -1,38 +1,18 @@
 from hashlib import *
-from random import randint
-from multiprocessing import Process
 import time
+from random import randint
+import multiprocessing
 
 
 def getPassword():
-    pword = sha256(b"ZZZ9999").hexdigest()
+    pword = input("lag et passord i formatet uuudddd: ")
+    pword = sha256(bytes(pword, "utf-8")).hexdigest()
     return pword
 
 
-hxUserPw = getPassword()
-print(hxUserPw)
-
-
 def Bruteforce(hx):
-    usrPw = b""
-    u = "AAA"
-    d = "0000"
-    print("running 1")
-
-    while sha256(usrPw).hexdigest() != hx:
-        pos = randint(0, 2)
-        u = u[:pos] + chr(randint(65, 90)) + u[pos + 1 :]
-
-        pos = randint(0, 3)
-        d = d[:pos] + chr(randint(48, 57)) + d[pos + 1 :]
-
-        usrPw = bytes(u + d, "utf-8")
-    print(usrPw)
-
-
-def Bruteforce2(hx):
-    usrPw = "AAA0000"
-    print("running 2")
+    usrPw = ""
+    print("master hacking 1 in progress")
     for a in range(ord("A"), ord("Z") + 1):
         usrPw = usrPw[:0] + chr(a) + usrPw[1:]
         for b in range(ord("A"), ord("Z") + 1):
@@ -48,4 +28,37 @@ def Bruteforce2(hx):
                             for g in range(ord("0"), ord("9") + 1):
                                 usrPw = usrPw[:6] + chr(g) + usrPw[7:]
                                 if sha256(bytes(usrPw, "utf-8")).hexdigest() == hx:
+                                    print("1 " + usrPw)
                                     return usrPw
+
+
+def Bruteforce2(hx):
+    usrPw = ""
+    print("master hacking 2 in progress")
+    for a in reversed(range(ord("A"), ord("Z") + 1)):
+        usrPw = usrPw[:0] + chr(a) + usrPw[1:]
+        for b in reversed(range(ord("A"), ord("Z") + 1)):
+            usrPw = usrPw[:1] + chr(b) + usrPw[2:]
+            for c in reversed(range(ord("A"), ord("Z") + 1)):
+                usrPw = usrPw[:2] + chr(c) + usrPw[3:]
+                for d in reversed(range(ord("0"), ord("9") + 1)):
+                    usrPw = usrPw[:3] + chr(d) + usrPw[4:]
+                    for e in reversed(range(ord("0"), ord("9") + 1)):
+                        usrPw = usrPw[:4] + chr(e) + usrPw[5:]
+                        for f in reversed(range(ord("0"), ord("9") + 1)):
+                            usrPw = usrPw[:5] + chr(f) + usrPw[6:]
+                            for g in reversed(range(ord("0"), ord("9") + 1)):
+                                usrPw = usrPw[:6] + chr(g) + usrPw[7:]
+                                if sha256(bytes(usrPw, "utf-8")).hexdigest() == hx:
+                                    print("2 " + usrPw)
+                                    return usrPw
+
+
+if __name__ == "__main__":
+    hxusrpw = getPassword()
+
+    p1 = multiprocessing.Process(target=Bruteforce, args=[hxusrpw])
+    p2 = multiprocessing.Process(target=Bruteforce2, args=[hxusrpw])
+
+    p1.start()
+    p2.start()
